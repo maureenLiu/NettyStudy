4a707a290b5d68375e28e99a31edad26693f636b
Maureen <1437867946@qq.com>
2020-03-20 16:01:39 +0800

    客户端窗口显示完毕后连接服务器

---
+++ src/main/java/com/maureen/netty/s03/ClientFrame.java
@@ -0,0 +1,32 @@
+package com.maureen.netty.s03;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClientFrame extends Frame {
+    TextArea ta = new TextArea(); //多行文本
+    TextField tf = new TextField(); //单行文本
+
+    public ClientFrame() {
+        this.setSize(600, 400);
+        this.setLocation(100, 20);
+        this.add(ta, BorderLayout.CENTER);
+        this.add(tf, BorderLayout.SOUTH);
+        tf.addActionListener(new ActionListener() { //按回车会触发actionPerformed
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                //把字符串发送到服务器
+                ta.setText(ta.getText() + tf.getText());
+                tf.setText("");
+            }
+        });
+        this.setVisible(true);
+        //窗口显示完毕后连接服务器
+        new Client().connect();
+    }
+
+    public static void main(String[] args) {
+        new ClientFrame();
+    }
+}
---
+++ src/main/java/com/maureen/netty/s03/Server.java
@@ -0,0 +1,54 @@
+package com.maureen.netty.s03;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+
+public class Server {
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(2);
+
+        ServerBootstrap sb = new ServerBootstrap();
+
+        try {
+            ChannelFuture future = sb.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ServerChannelInitializer())
+                    .bind(8888);
+            future.sync();
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ServerChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ch.pipeline().addLast(new ServerChannelHandler());
+    }
+}
+
+class ServerChannelHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = null;
+        try {
+            buf = (ByteBuf) msg;
+            byte[] bytes = new byte[buf.readableBytes()];
+            buf.getBytes(buf.readerIndex(), bytes);
+            System.out.println(new String(bytes));
+
+        } finally {
+            if (buf != null)
+                ReferenceCountUtil.release(buf);
+        }
+    }
+}
